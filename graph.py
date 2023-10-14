18ad90ee97dcdae87e53be1a0c967aa3b60dc23c
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-10-14 02:47:56 -0400

    Random Graph Creation ISS#1
    Completed `create_random_graph` function as specified in the lab instructions.

--- graph.py
+++ graph.py
@@ -1,4 +1,7 @@
 # Undirected graph using an adjacency list
+import random
+
+
 class Graph:
 
     def __init__(self, n):
@@ -22,3 +25,33 @@ class Graph:
 
     def number_of_nodes(self):
         return len(self.adj)
+
+
+def create_random_graph(num_nodes: int, num_edges: int) -> Graph:
+    # You can't have a graph with a non-positive number of nodes/edges
+    num_nodes, num_edges = max(num_nodes, 1), max(num_edges, 0)
+    # And undirected graphs can have a maximum of (n * (n - 1)) / 2 edges
+    num_edges = min(num_edges, (num_nodes ** 2 - num_nodes) // 2)
+    g = Graph(num_nodes)
+    # Use a set to track all the edges added so far
+    existing_edges = set()
+    count = 0
+    while count < num_edges:
+        # In an undirected graph, an edge (u, v) can always be represented such that u < v. So we only need to
+        # track that the randomly generated numbers are not equal (no self-loops), and then we can swap values
+        # if the generated v is less than the generated u. Then we can check if this edge has already been
+        # created, and act accordingly. The `are_connected` function could be used, but since adjacency is done
+        # using a Python list, that is an O(n) operation. That could be fixed by using a set for adjacency
+        # instead.
+        u, v = random.randint(0, num_nodes), random.randint(0, num_nodes)
+        if u == v:
+            continue
+        elif u > v:
+            u, v = v, u
+        if (u, v) in existing_edges:
+            continue
+        g.add_edge(u, v)
+        existing_edges.add((u, v))
+        count += 1
+
+    return g
