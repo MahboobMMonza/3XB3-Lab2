2cc3af2d33412e82d5673f7feea7a410f14a2054
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2023-10-15 23:40:05 -0400

    Experiment 2 Skeleton ISS#5
    All the workings required to conduct experiment 2, though values may need to be changed. Resolves #5 .

---
+++ experiment2.py
@@ -0,0 +1,28 @@
+from enhanced_traversals import is_connected
+from utilities import *
+
+
+def run_experiment2() -> None:
+    num_nodes = [201, 401]
+    benchmarks, reps, include_start = 10, 40, True
+    legend_labels = [f'{nodes} Node Graph' for nodes in num_nodes]
+    title = 'Proportion of Edges vs Connectivity Probability'
+    desc = (f'{benchmarks + include_start} benchmarks from (n - 1) edges for n nodes with {reps} repetitions'
+            f' per edge count, proportional to total possible edges in the graph')
+    x_label = 'Proportion of Edges (from minimum number edges needed for connectivity)'
+    y_label = 'Probability of Graph Connectivity'
+    results = [(prp, prob) for prp, prob in incrementing_node_graph_evals(num_nodes,
+                                                                          lambda n: n - 1,
+                                                                          lambda n: ((n ** 2 - n) // 2) // 5,
+                                                                          benchmarks,
+                                                                          reps,
+                                                                          include_start,
+                                                                          is_connected,
+                                                                          true_max_eval=lambda n: (n ** 2 - n) // 2)]
+    proportion = results[0][0]
+    probability = [res[1] for res in results]
+    create_plot(proportion, probability, legend_labels, title, desc, x_label, y_label, 1.60, False)
+
+
+if __name__ == '__main__':
+    run_experiment2()
